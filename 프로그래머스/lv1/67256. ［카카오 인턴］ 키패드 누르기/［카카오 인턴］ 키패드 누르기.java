e23a680472e68c4f964fd2a566ac0aea1d269d58
LEEGIHO94 <116015708+LEEGIHO94@users.noreply.github.com>
2022-12-05 20:59:22 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.29 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,18 +1,18 @@
 class Solution {
-    public String solution(int[] numbers, String hand) {
-        String answer ="";
+    public static String solution(int[] numbers, String hand) {
+        StringBuilder answer = new StringBuilder();
         int leftindex = 10;
         int rightindex = 12;
-        
+        //0을 11로 바꾸면 모든 숫자들이 3의 배수 혹은 나머지로 표현이 가능하다는 점이중요하다
         for(int n : numbers){
             if(n==1||n==4||n==7){
-                answer+="L";
+                answer.append("L");
                 leftindex = n;
-                
+
             }else if(n==3||n==6||n==9){
-                answer+="R";
+                answer.append("R");
                 rightindex = n;
-                
+
             }else{
                 if(n==0){
                     n+=11;
@@ -21,21 +21,23 @@ class Solution {
                 int rd = (Math.abs(n-rightindex))/3 + (Math.abs(n-rightindex))%3;
                 if(ld==rd){
                     if(hand.equals("right")){
-                        answer+="R";
+                        answer.append("R");
                         rightindex = n;
                     }else{
-                        answer+="L";
+                        answer.append("L");
                         leftindex = n;
                     }
                 }else if(ld>rd){
-                    answer+="R";
+                    answer.append("R");
                     rightindex = n;
                 }else{
-                    answer+="L";
+                    answer.append("L");
                     leftindex = n;
                 }
             }
         }
-        return answer;
+        return answer.toString();
     }
 }
+
+
