4effed56aa4cc824b2ee60319b897c7c2188db3d
LEEGIHO94 <116015708+LEEGIHO94@users.noreply.github.com>
2022-12-15 20:10:20 +0900

    [unrated] Title: 옹알이 (2), Time: 0.62 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,39 +1,33 @@
 class Solution {
     public static int solution(String[] babbling) {
         int answer = 0;
-
         String[] crying = {"aya","ye","woo","ma"};
+        String[] dontCrying =  {"ayaaya","yeye","woowoo","mama"};
+        //일단 dontCrying 으로 값을 변경해준다.  1
+        //담은 값에서 crying 값을 변경해준다.k
+        //결과를 분류 할 때 값이 전부 1인 것들만 체크
 
         for (String test : babbling) {
-            int count = 0;
-
-            for (int i = 0 ; i < crying.length ; i++) {
-                test = test.replace(crying[i],String.valueOf(i));
-            }
+            boolean count = false;
+            for (int i = 0; i < crying.length; i++) {
+                test = test.replace(dontCrying[i], "k");
+                test = test.replace(crying[i],"0");
+            }//순서가 중요하다.
 
             for (int i = 0; i < test.length(); i++) {
-                if(test.charAt(i) == '0' || test.charAt(i) == '1'
-                        || test.charAt(i) == '2' || test.charAt(i) == '3'){
-                    if(i != 0) {
-                        if (test.charAt(i) == test.charAt(i - 1)) {
-                            count = 0;
-                            break;
-                        }
-                        
-                    }
-                    else count = 1;
-
-                }else {
-                    count =0;
+                if(test.charAt(i)!= '0') {
+                    count = true;
                     break;
                 }
+
             }
-            if(count ==1  )
-                answer++;
+            if(!count) answer++;
+
+
+
+
         }
-
-
-
         return answer;
+
     }
 }
