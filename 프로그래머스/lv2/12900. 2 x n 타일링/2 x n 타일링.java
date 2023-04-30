83fb4e57f29a2a9a5e1d98795e21df20d9d8665c
SigLee <116015708+LEEGIHO94@users.noreply.github.com>
2023-05-01 01:10:47 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.46 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,20 +1,17 @@
 import java.util.*;
 
-class Solution {    
+class Solution {
+    
     public int solution(int n) {
-        int answer = 0;
-        //0번지의 값 추가 -> 인덱스 일치 작업
         int[] memo = new int[n+1];
-        
+
+        //1번지 2번지 추가
         memo[1] = 1;
         memo[2] = 2;
 
         for(int i = 3 ; i <= n ; i++){
-            memo[i] = (memo[i-1] + memo[i-2]) %1000000007;
+            memo[i] = (memo[i-1] + memo[i-2])%1000000007 ;
         }
-        
-        
-        
         return memo[n];
     }
     
