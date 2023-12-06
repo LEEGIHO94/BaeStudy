95aac6e4a8beb677d6a01bec61809e24a60d748c
SigLee <116015708+LEEGIHO94@users.noreply.github.com>
2023-12-06 10:25:17 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,9 +1,8 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    static Integer[] memo = new Integer[12];
+    static int[] memo = new int[12];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,16 +16,19 @@ public class Main {
         }
     }
 
-    private static Integer dp(int N) {
-        if (memo[N] == null) {
-            if (N > 3) {
-                memo[N] = dp(N - 3) + dp(N - 2) + dp(N - 1);
-            } else if (N > 2) {
-                memo[N] = dp(N - 2) + dp(N - 1);
-            } else if (N > 1) {
-                memo[N] = dp(N - 1);
-            }
+    public static int dp(int n){
+        if (memo[n] != 0) {
+            return memo[n];
         }
-        return memo[N];
+        if (n > 3) {
+            return dp(n - 3) + dp(n - 2) + dp(n - 1);
+        }
+        if(n > 2){
+            return dp(n - 2) + dp( n - 1);
+        }
+        if (n > 1) {
+            return dp(n - 1);
+        }
+        return memo[n];
     }
 }
