b9161a5724cf5fbec4207962025a195f2fb117e0
SigLee <116015708+SigLee2247@users.noreply.github.com>
2024-03-12 16:46:14 +0900

    [level 2] Title: 숫자의 표현, Time: 1.14 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 1 ; i <= n ; i++){
+            int tmp = 0;
+            for(int j = i ; j <= n ; j++){
+                tmp += j;
+                if(tmp > n){
+                    break;
+                }
+                if(tmp == n){
+                    answer++;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
+//주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수는 주어진 수의 홀수 약수의 개수와 같다라는 정수론 정리
