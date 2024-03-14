aebc358e34475fd77953880a2c95cdc404bd3f11
SigLee <116015708+SigLee2247@users.noreply.github.com>
2024-03-14 11:36:53 +0900

    [level 2] Title: 피보나치 수, Time: 11.46 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,23 +1,17 @@
-import java.util.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 class Solution {
-    Integer[] memo = new Integer[100001];
-    
-    public int solution(int n) {
-        init();
-        return dp(n);
-    }
-    
-    public void init(){
-        memo[0] = 0;
-        memo[1] = 1;
-    }
-    
-    public int dp(int n){
-        if(n >= 0 && memo[n] != null)
-            return memo[n];
-        
-        memo[n] =  (dp(n - 2) + dp(n - 1)) % 1234567 ;
-        return memo[n];
+        public static int solution(int n) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(0,1));
+
+        if (n > 1) {
+            for (int i = 2; i <= n; i++) {
+                list.add((list.get(i-1) + list.get(i-2)) % 1234567);
+            }
+        }
+
+        return list.get(n);
     }
 }
