acb0523de5bf5ef4e2ec76089fe549d267aa5fd1
SigLee <116015708+SigLee2247@users.noreply.github.com>
2024-04-16 02:44:29 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 15748 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,26 +1,27 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] cArray = br.readLine().toCharArray();
-        Stack<Character> stack = new Stack<>();
-        int i = 0;
-        int result = 0;
-        while (cArray.length > i) {
-            if (cArray[i] == '(' && cArray[i + 1] == ')') {
-                result += stack.size();
-                i+=1;
-            } else if (cArray[i] == '(')
-                stack.push(cArray[i]);
-            else if (cArray[i] == ')') {
-                stack.pop();
-                result += 1;
-            }
-            i++;
-        }
 
-        System.out.println(result);
+        //init
+        int l = 0 , ans = 0;
+        boolean f = false;
+
+        for (char c : br.readLine().toCharArray()) {
+            if (c == '(') {
+                l++;
+                f = false;
+            }
+            else{
+                l--;
+                if(!f) ans+=l;
+                else ans++;
+                f = true;
+            }
+        }
+        System.out.println(ans);
     }
 }
